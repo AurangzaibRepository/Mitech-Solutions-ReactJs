72f03a03846af28acf2bf0e6594e2ed13f0c32fc
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-14 23:30:51 +0500

    Writes test case for inex

---
+++ src/components/About/Index.test.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from './index';
+
+test('Test about', () => {
+  render(<About />);
+
+  const element = screen.getByText(/About/);
+
+  expect(element).toBeInTheDocument();
+});
