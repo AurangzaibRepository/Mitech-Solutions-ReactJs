62066fca5d2cf800417480545d7e1164576f621b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 22:33:59 +0500

    Adds mockdata

--- src/components/About/tests/Statistics.test.jsx
+++ src/components/About/tests/Statistics.test.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Statistics from '../Statistics';
+
+const mockData = [
+  {
+    id: 1,
+    title: 'Completed Projects',
+    count: 2,
+  },
+  {
+    id: 2,
+    title: 'Programming',
+    count: 4,
+  },
+];
