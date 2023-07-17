76955a3807f812f13b68f5a9d703ed85bf699d4e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 20:38:20 +0500

    Creates mock data

--- src/components/About/tests/Mission.Test.jsx
+++ src/components/About/tests/Mission.Test.jsx
@@ -1,1 +1,11 @@
-import Ract from 'react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Mission from '../Mission';
+
+const mockData = {
+  mission: 'Our mission is to empower businesses with their technology by providing superior service, expertise and innovative products that satisfy the needs and exceed the expectations of our customers We have stayed true to our core values over the years and have created a company that our employees and clients are proud to associate with.',
+  vision: 'To build a better world with Information Technology and most trusted IT advisor and key partner to our clients that recognized by our clients for delivering excellence.',
+};
+
+test('Test mission', () => {
+});
