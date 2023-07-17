efdd7dd9d9df53fb4bc88e81382031e1faa4e094
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 23:00:29 +0500

    Write assertion

--- src/components/About/tests/Customers.test.jsx
+++ src/components/About/tests/Customers.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Customers from '../Customers';
+
+test('Test customers', () => {
+  render(<Customers />);
+
+  const element = screen.getByText('Our Customers');
+
+  expect(element).toBeInTheDocument();
+});
