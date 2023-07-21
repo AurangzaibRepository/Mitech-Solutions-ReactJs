fda1dfe67e14a97f5970293167ec6399bf127804
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-21 22:31:50 +0500

    Implements test function

--- src/components/Contact/tests/Index.test.jsx
+++ src/components/Contact/tests/Index.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Contact from '../index';
+
+test('Test contact', () => {
+  render(<Contact />);
+
+  const element = screen.getByText('Contact us');
+
+  expect(element).toBeInTheDocument();
+});
