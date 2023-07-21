2ee084ae767462dcb3d905da1642b8f263692f63
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-21 23:21:09 +0500

    Adds mock data

--- src/components/Contact/tests/ContactInformation.test.jsx
+++ src/components/Contact/tests/ContactInformation.test.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ContactInformation from '../ContactInformation';
+
+const mockData = {
+  email: 'mitech2u@gmail.com',
+  phone_number: '+60122163731',
+  whatsapp_number: '+60122163731',
+};
+
+test('test contact information', () => {
+});
