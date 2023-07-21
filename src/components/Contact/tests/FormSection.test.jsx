aea0f48658adf36a8e13a97dae77ba2ca95b0c3d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-21 22:38:04 +0500

    Implements test function

--- src/components/Contact/tests/FormSection.test.jsx
+++ src/components/Contact/tests/FormSection.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import FormSection from '../FormSection';
+
+test('Test form section', () => {
+  render(<FormSection />);
+
+  const element = screen.getByText('Write a message');
+
+  expect(element).toBeInTheDocument();
+});
