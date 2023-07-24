d15baabc8ac65d8409d9b27a4b6a00796ecc24bf
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 20:06:36 +0500

    Implements test for font awesome icon tag by title

--- src/components/Contact/tests/FormSection.test.jsx
+++ src/components/Contact/tests/FormSection.test.jsx
@@ -6,11 +6,12 @@ import FormSection from '../FormSection';
 test('Test form section', () => {
   render(<FormSection />);
 
-  const element = screen.getByText('Write a message');
+  let element = screen.getByText('Write a message');
+  expect(element).toBeInTheDocument();
 
+  element = screen.getByTitle('Facebook icon');
   expect(element).toBeInTheDocument();
 
   const anchor = screen.getAllByRole('link');
-
   expect(anchor[0]).toHaveAttribute('href', 'https://www.facebook.com');
 });
