5ebb5b58d26eb53b8ebe05643d0d71c4539205d1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 20:12:52 +0500

    Implements element testing

--- src/components/Contact/tests/Form.test.jsx
+++ src/components/Contact/tests/Form.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Form from '../Form';
+
+test('Test form', () => {
+  render(<Form />);
+
+  const element = screen.getByText('Name');
+
+  expect(element).toBeInTheDocument();
+});
