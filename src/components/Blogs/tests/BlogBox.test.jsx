18b3d2e933a3995407aefafc03274527afb47ede
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 21:44:06 +0500

    Writes assertions for fields

--- src/components/Blogs/tests/BlogBox.test.jsx
+++ src/components/Blogs/tests/BlogBox.test.jsx
@@ -1,16 +1,30 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import BlogBox from '../BlogBox';
+import blogImage1 from '../../../assets/blogs/blog1.jpg';
 
-const mockData = [
-  {
-    id: 1,
-    title: 'How to get Windows 11 for free',
-    description: 'Technically, Windows 11 isn’t free.',
-  },
-  {
-    id: 2,
-    title: 'Should you upgrade to Windows 11? Here’s a list of missing features',
-    description: 'Windows 11 is here and it’s now widely available for the majority of the users.',
-  },
-];
+const mockData = {
+  title: 'How to get Windows 11 for free',
+  description: 'Technically, Windows 11 isn’t free.',
+};
+
+test('Test blog box', () => {
+  render(<BlogBox
+    image={blogImage1}
+    title={mockData.title}
+    description={mockData.description}
+  />);
+
+  expect(
+    screen.getByText(/How to get Windows/),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText(/Technically, Windows 11/),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByRole('button'),
+  ).toBeInTheDocument();
+});
