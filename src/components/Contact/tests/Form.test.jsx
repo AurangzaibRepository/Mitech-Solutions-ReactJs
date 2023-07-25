5b5dd0ca653e09315441ec99a145d48da381aebd
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 21:08:54 +0500

    Writes assertion for successful form

--- src/components/Contact/tests/Form.test.jsx
+++ src/components/Contact/tests/Form.test.jsx
@@ -4,6 +4,7 @@ import {
   render,
   screen,
   fireEvent,
+  waitFor,
 } from '@testing-library/react';
 import Form from '../Form';
 
@@ -28,8 +29,42 @@ it('Render fields', () => {
 
 it('Submit failure', async () => {
   const { container } = render(<Form />);
-  const spans = await container.getElementsByClassName('spn-error');
 
   fireEvent.submit(screen.getByRole('button'));
-  expect(spans.length).toBe(4);
+
+  const spans = await container.getElementsByClassName('spn-error');
+  await waitFor(() => expect(spans.length).toBe(4));
+});
+
+it('Submit success', async () => {
+  const { container } = render(<Form />);
+
+  fireEvent.input(screen.getByRole('textbox', { name: 'Name:' }), {
+    target: {
+      value: 'John',
+    },
+  });
+
+  fireEvent.input(screen.getByRole('textbox', { name: 'Email:' }), {
+    target: {
+      value: 'john@gmail.com',
+    },
+  });
+
+  fireEvent.input(screen.getByRole('textbox', { name: 'Subject:' }), {
+    target: {
+      value: 'Enquiry subject',
+    },
+  });
+
+  fireEvent.input(screen.getByRole('textbox', { name: 'Message:' }), {
+    target: {
+      value: 'Test message',
+    },
+  });
+
+  fireEvent.submit(screen.getByRole('button'));
+
+  const spans = await container.getElementsByClassName('spn-error');
+  await waitFor(() => expect(spans.length).toBe(0));
 });
