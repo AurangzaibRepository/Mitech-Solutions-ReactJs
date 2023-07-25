8dcc3af37f94fd495c80005b3a405a6336b8798d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 21:27:00 +0500

    Writes asstion for label

--- src/components/Blogs/tests/Index.test.jsx
+++ src/components/Blogs/tests/Index.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Blogs from '../index';
+
+test('Test blogs', () => {
+  render(<Blogs />);
+
+  expect(
+    screen.getByText('Blogs'),
+  ).toBeInTheDocument();
+});
