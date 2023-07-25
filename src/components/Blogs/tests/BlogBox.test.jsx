e4ca041f3e7826c05d3c063ed3052ecd2fe790d1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 21:33:49 +0500

    Defines mock data

--- src/components/Blogs/tests/BlogBox.test.jsx
+++ src/components/Blogs/tests/BlogBox.test.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import BlogBox from '../BlogBox';
+
+const mockData = [
+  {
+    id: 1,
+    title: 'How to get Windows 11 for free',
+    description: 'Technically, Windows 11 isn’t free.',
+  },
+  {
+    id: 2,
+    title: 'Should you upgrade to Windows 11? Here’s a list of missing features',
+    description: 'Windows 11 is here and it’s now widely available for the majority of the users.',
+  },
+];
