328d7c5bca30fa403649efeade20c7c0d6a7bd26
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-26 20:58:35 +0500

    Writes assertion for text fiedl

--- src/components/Portfolio/tests/Index.test.jsx
+++ src/components/Portfolio/tests/Index.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Portfolio from '../index';
+
+test('Test portfolio', () => {
+  render(<Portfolio />);
+
+  expect(
+    screen.getByText('Take a look at our Portfolio'),
+  ).toBeInTheDocument();
+});
