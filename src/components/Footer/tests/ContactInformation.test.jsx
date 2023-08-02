63f612858802d4338dadab3cf824a2437233e297
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 21:21:52 +0500

    Renders component

--- src/components/Footer/tests/ContactInformation.test.jsx
+++ src/components/Footer/tests/ContactInformation.test.jsx
@@ -6,3 +6,7 @@ const mockData = {
   address: 'MITECH SOLUTIONS\nNo 7, Jalan HP 1/8, Hill Park\n43500, Semenyih, Selangor.',
   contact_number: '+60122163731',
 };
+
+test('Test contact information', () => {
+  render(<ContactInformation data={mockData} />);
+});
