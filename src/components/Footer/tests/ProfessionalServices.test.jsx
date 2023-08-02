363bc70a6828bc1042da26cf5657f0893a585214
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 23:16:45 +0500

    Adds mock data

--- src/components/Footer/tests/ProfessionalServices.test.jsx
+++ src/components/Footer/tests/ProfessionalServices.test.jsx
@@ -5,3 +5,22 @@ import {
   within,
 } from '@testing-library/react';
 import ProfessionalServices from '../ProfessionalServices';
+
+const mockData = [
+  {
+    id: 1,
+    property: 'Website Development',
+  },
+  {
+    id: 2,
+    property: 'Software Development',
+  },
+  {
+    id: 3,
+    property: 'Mobile Apps Development',
+  },
+  {
+    id: 4,
+    property: 'SEO',
+  },
+];
