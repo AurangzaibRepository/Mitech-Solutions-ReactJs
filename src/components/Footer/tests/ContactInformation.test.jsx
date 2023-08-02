90b5006eca95289b27ca2959a1c3bc63d67969a2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 21:34:16 +0500

    Writes assertions for text elements

--- src/components/Footer/tests/ContactInformation.test.jsx
+++ src/components/Footer/tests/ContactInformation.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ContactInformation from '../ContactInformation';
@@ -9,4 +10,24 @@ const mockData = {
 
 test('Test contact information', () => {
   render(<ContactInformation data={mockData} />);
+
+  expect(
+    screen.getByText('Contact Information'),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText('Our address:'),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText('Call us:'),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText(/MITECH SOLUTIONS No 7, Jalan HP 1\/8/),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText('+60122163731'),
+  ).toBeInTheDocument();
 });
