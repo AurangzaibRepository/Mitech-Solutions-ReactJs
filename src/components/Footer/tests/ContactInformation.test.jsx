4b4382e1f0244301c9b1c34210b61e235256c36c
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 21:20:54 +0500

    Adds mock data

--- src/components/Footer/tests/ContactInformation.test.jsx
+++ src/components/Footer/tests/ContactInformation.test.jsx
@@ -1,3 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ContactInformation from '../ContactInformation';
+
+const mockData = {
+  address: 'MITECH SOLUTIONS\nNo 7, Jalan HP 1/8, Hill Park\n43500, Semenyih, Selangor.',
+  contact_number: '+60122163731',
+};
