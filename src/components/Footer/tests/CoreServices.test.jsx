1a61577b9c9f599c573e6701f17f541878ab9510
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 21:39:44 +0500

    Adds mock da ta

--- src/components/Footer/tests/CoreServices.test.jsx
+++ src/components/Footer/tests/CoreServices.test.jsx
@@ -1,3 +1,18 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CoreServices from '../CoreServices';
+
+const MockData = [
+  {
+    id: 1,
+    property: 'Refurbished Desktops',
+  },
+  {
+    id: 2,
+    property: 'Refurbished Laptops',
+  },
+];
+
+test('Test core services', () => {
+});
