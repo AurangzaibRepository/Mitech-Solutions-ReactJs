ef92a84adf3e06314299eb4a32a6b06e5acef8d3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-07 22:42:37 +0500

    Implements assertion for multiple images

--- src/components/Home/tests/SliderSection.test.jsx
+++ src/components/Home/tests/SliderSection.test.jsx
@@ -1,3 +1,11 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import SliderSection from '../SliderSection';
+
+test('Test slider section', () => {
+  render(<SliderSection />);
+
+  const imageList = screen.getAllByRole('img');
+  imageList.forEach((image) => expect(image).toHaveAttribute('alt', 'Product'));
+});
