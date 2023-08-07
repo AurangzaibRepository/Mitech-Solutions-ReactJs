106df3f963fce49ab93a21a1c2c3d93ed3c624e2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-07 19:03:18 +0500

    Implements test for label

--- src/components/Navbar/tests/Index.test.jsx
+++ src/components/Navbar/tests/Index.test.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
-import { render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Navbar from '..';
+
+test('Test Navbar', () => {
+  render(<Navbar />);
+
+  expect(
+    screen.getByText('MiTech Solutions'),
+  ).toBeInTheDocument();
+});
