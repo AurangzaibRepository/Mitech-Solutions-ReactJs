7b00ae544e8c24e7c3d3436626280fab19dc2cf9
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-07 20:33:13 +0500

    Implements assertion for servies link

--- src/components/Navbar/tests/Index.test.jsx
+++ src/components/Navbar/tests/Index.test.jsx
@@ -1,12 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from '..';
 
 test('Test Navbar', () => {
-  render(<Navbar />);
+  render(<BrowserRouter><Navbar /></BrowserRouter>);
+
+  userEvent.click(screen.getByText('Services'));
 
   expect(
     screen.getByText('MiTech Solutions'),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText('Services'),
   ).toBeInTheDocument();
 });
